f34354fd51f31c893a11a11fd2e400f8212acd14
Albert Camilo <camiloblancoamado@gmail.com>
2026-08-01 16:02:44 -0500

    refactor: update settings and urls files, remove comments; enhance index view in noticias

--- noticias/views.py
+++ noticias/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Noticia
+from django.shortcuts import render
+from catalogo.models import Libro
+from noticias.models import Noticia
 
 
-def listado(request):
-    return render(request, "noticias/listado.html", {
-        "noticias": Noticia.objects.all(),
-        "categorias": Noticia.Categoria.choices,
+def index(request):
+    return render(request, "home/index.html", {
+        "page_title": "Bajo el Volcán Editorial Literaria",
+        "description": "Editorial literaria independiente. Descubre nuestro catálogo de novelas, cuentos y poesía de autores colombianos y latinoamericanos.",
+        "libros": Libro.objects.all(),
+        "slides": Libro.objects.exclude(imagen_hero="").exclude(imagen_hero__isnull=True),
+        "noticias": Noticia.objects.all().order_by('-fecha'),
     })
-
-
-def detalle(request, slug):
-    noticia = get_object_or_404(Noticia, slug=slug)
-    return render(request, "noticias/detalle.html", {"noticia": noticia})
